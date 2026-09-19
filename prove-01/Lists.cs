6684326ef624d4e7979b90276b0c60ef6020a372
JoshuaCF-silv <flo22018@byui.edu>
2026-09-19 01:14:29 -0600

    Made Prove-01 changes

---
+++ prove-01/Lists.cs
@@ -0,0 +1,59 @@
+namespace prove_01;
+
+public class Lists
+{
+    /// <summary>
+    /// This function will produce a list of size 'length' starting with 'number' followed by multiples of 'number'.  For 
+    /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
+    /// integer greater than 0.
+    /// </summary>
+    /// <returns>array of doubles that are the multiples of the supplied number</returns>
+    public static double[] MultiplesOf(double number, int length)
+    {
+        // TODO Problem 1 Start (don't forget to fill out the 01-prove-response.docx)
+        double[] numArray = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            numArray[i] = (i+1) * number;
+        }
+        return numArray; // replace this return statement with your own
+    }
+    
+    /// <summary>
+    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
+    /// <c>&lt;List&gt;{1, 2, 3, 4, 5, 6, 7, 8, 9}</c> and an amount is 3 then the list returned should be 
+    /// <c>&lt;List&gt;{7, 8, 9, 1, 2, 3, 4, 5, 6}</c>.  The value of amount will be in the range of <c>1</c> and 
+    /// <c>data.Count</c>.
+    /// <br /><br />
+    /// Because a list is dynamic, this function will modify the existing <c>data</c> list rather than returning a new list.
+    /// </summary>
+    public static void RotateListRight(List<int> data, int amount)
+    {
+        // TODO Problem 2 Start (don't forget to fill out the 01-prove-response.docx)
+        int index = 0;
+        while (index < amount)
+        {
+            int saveEvenValue = 0;
+            int saveOddValue = data[0];
+            int saveLastValue = data[data.Count - 1];
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (i == data.Count-1)
+                {
+                    data[0] = saveLastValue;
+                }
+                else if (i % 2 == 0)
+                {
+                    saveEvenValue = data[i + 1];
+                    data[i + 1] = saveOddValue;
+                }
+                else if  (i % 2 == 1)
+                {
+                    saveOddValue = data[i + 1];
+                    data[i + 1] = saveEvenValue;
+                }
+            }
+            index++;
+        }
+    }
+}
